1bfa0df19dfd129c32831721b0c83c9dd586c3ab
Divyansh Verma <divyanshverma@Divyanshs-MacBook-Pro.local>
2025-02-11 16:08:44 +0530

    final-commit

--- consumer.js
+++ consumer.js
@@ -1,7 +1,7 @@
 const { kafka } = require("./client");
-
+const group = process.argv[2];
 async function init() {
-  const consumer = kafka.consumer({ groupId: "group-1" });
+  const consumer = kafka.consumer({ groupId: group });
   consumer.connect();
 
   await consumer.subscribe({
@@ -11,7 +11,9 @@ async function init() {
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
-      console.log(`[${topic}]: PART:${partition}: ${message.value.toString()}`);
+      console.log(
+        `${group}: [${topic}]: PART:${partition}: ${message.value.toString()}`
+      );
     },
   });
 }
--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function init() {
   const producer = kafka.producer();
@@ -6,17 +12,23 @@ async function init() {
   await producer.connect();
   console.log("Producer connected");
 
-  await producer.send({
-    topic: "rider-updates",
-    messages: [
-      {
-        key: "location-update",
-        value: JSON.stringify({ name: "Tony Stark", loc: "SOUTH" }),
-      },
-    ],
-  });
+  rl.setPrompt("> ");
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
 
-  await producer.disconnect();
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name: riderName, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+  });
 }
 
 init();
