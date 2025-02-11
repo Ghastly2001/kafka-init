eaced0d1f2ea6bde22cbb4c75895a7dbc5910a14
Divyansh Verma <divyanshverma@Divyanshs-MacBook-Pro.local>
2025-02-11 15:45:38 +0530

    added consumer

--- consumer.js
+++ consumer.js
@@ -3,4 +3,17 @@ const { kafka } = require("./client");
 async function init() {
   const consumer = kafka.consumer({ groupId: "group-1" });
   consumer.connect();
+
+  await consumer.subscribe({
+    topics: ["rider-updates"],
+    fromBeginning: true,
+  });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message, heartbeat, pause }) => {
+      console.log(`[${topic}]: PART:${partition}: ${message.value.toString()}`);
+    },
+  });
 }
+
+init();
